22eef3e9383402aeff9b146ea794bf8fc001eadf
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-04-17 00:44:20 +0900

    6

---
+++ elb.tf
@@ -0,0 +1,30 @@
+resource "aws_elb" "mts-elb" {
+  name = "mts-elb"
+  subnets = [
+    "${aws_subnet.elb-1a.id}",
+    "${aws_subnet.elb-1c.id}",
+  ]
+
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/_ping"
+    interval = 5
+  }
+
+  cross_zone_load_balancing = true
+
+  tags {
+    Name = "mts-elb"
+  }
+}
