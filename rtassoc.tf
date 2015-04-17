7e2dc433d6392269764952232e2cdb7e4229755a
TANABE Ken-ichi <kenichi.tanabe@hde.co.jp>
2015-04-17 10:09:02 +0900

    5.1

--- rtassoc.tf
+++ rtassoc.tf
@@ -3,17 +3,7 @@ resource "aws_route_table_association" "public-elb-1a" {
   route_table_id = "${aws_route_table.public.id}"
 }
 
-resource "aws_route_table_association" "public-elb-1c" {
-  subnet_id = "${aws_subnet.elb-1c.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
-
 resource "aws_route_table_association" "public-web-1a" {
   subnet_id = "${aws_subnet.web-1a.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
-
-resource "aws_route_table_association" "public-web-1c" {
-  subnet_id = "${aws_subnet.web-1c.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
